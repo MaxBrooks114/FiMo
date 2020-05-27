48ea211e50ee838bd3f1eb66ab1119f90a94d896
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-27 16:41:29 -0400

    get list of files and filetypes in chosen directory

--- Fimo.py
+++ Fimo.py
@@ -1,6 +1,55 @@
+from os import listdir
+from os.path import isfile, join
 import os
-import shutil
 
-pdfs = []
-csvs = []
+# global dict of files, key is  file types, values will be array or tuple of
+# files themselves.
+
+#create gui to list directories then based on directory list all extensions
+# in that directory
+
+#
+path = "/Users/maxbrooks/downloads"
+
+
+
+#create folders based on extension
+
+#populate folders with the files of that extension
+
+
+
+files = {}
+
+
+
+
+os.chdir(path)
+
+file_types = []
+
+files = [f for f in listdir(path) if isfile(join(path, f))]
+
+for file in files:
+  file_type = file.split(".")[-1]
+  if file_type not in file_types:
+      file_types.append(file_type)
+
+print(file_types)
+
+
+
+print(os.listdir(path))
+
+
+
+
+
+
+
+
+
+
+
+
 
