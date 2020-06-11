b942e330799bf400fb908772c3e9b9d97b021c9b
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-06-11 13:32:53 -0400

    removed globals with directory selector class

--- Fimo.py
+++ Fimo.py
@@ -6,68 +6,66 @@ from tkinter import filedialog, messagebox
 from pathlib import Path
 import shutil
 
+class Directory_Selector():
+    def __init__(self):
+        self.home = str(Path.home())
+        self.chosen_directory = self.home
 
-home = str(Path.home())
-chosen_directory = home
+    def choose_directory(self):
+        self.chosen_directory = tkinter.filedialog.askdirectory()
+        if not self.chosen_directory:
+            self.chosen_directory = self.home
+        directories_list.requery()
 
-
-def choose_directory():
-    global chosen_directory
-    chosen_directory = tkinter.filedialog.askdirectory()
-    if not chosen_directory:
-        chosen_directory = home
-    print(chosen_directory)
-    directories_list.requery()
-
-
-def create_folder():
-    print("Make a folder with these extensions dawg")
-    parent = join(chosen_directory, folders_list.link_value)
-    extension_index = extensions_list.curselection()[0]
-    extension = extensions_list.get(extension_index)
-    print(extension)
-    path = join(parent, extension)
-    print (path)
-    msg = tkinter.messagebox.askquestion(title=None, message="Are you sure "
-                                                             "you "
-                                                             "want "
-                                                             "to make a "
-                                                             "folder with "
-                                                             "all of your "
-                                                             "loose {} "
-                                                             "files? "
-                                                             "*note "
-                                                             "that if "
-                                                             "the "
-                                                             "folder "
-                                                             "already "
-                                                             "exists, "
-                                                             "the "
-                                                             "files "
-                                                             "will be "
-                                                             "moved "
-                                                             "into "
-                                                             "that "
-                                                             "folder".format(
-        extension))
-    if msg == "yes":
-        if extension in path and path not in listdir(parent):
-            os.mkdir(path)
-            print("folder created")
-            tkinter.messagebox.showinfo(title=None, message="A {} folder has "
-                                                        "been "
-                                                        "created.".format(
+    def create_folder(self):
+        print("Make a folder with these extensions dawg")
+        parent = join(self.chosen_directory,
+                      folders_list.link_value)
+        extension_index = extensions_list.curselection()[0]
+        extension = extensions_list.get(extension_index)
+        print(extension)
+        path = join(parent, extension)
+        print (path)
+        msg = tkinter.messagebox.askquestion(title=None, message="Are you sure "
+                                                                 "you "
+                                                                 "want "
+                                                                 "to make a "
+                                                                 "folder with "
+                                                                 "all of your "
+                                                                 "loose {} "
+                                                                 "files? "
+                                                                 "*note "
+                                                                 "that if "
+                                                                 "the "
+                                                                 "folder "
+                                                                 "already "
+                                                                 "exists, "
+                                                                 "the "
+                                                                 "files "
+                                                                 "will be "
+                                                                 "moved "
+                                                                 "into "
+                                                                 "that "
+                                                                 "folder".format(
             extension))
-        moved_file_list = []
-        for file in listdir(parent):
-            if file.split(".")[-1] == extension and isdir(path):
-                shutil.move(join(parent, file), path)
-                moved_file_list.append(file)
-                print("{} moved".format(file))
-        tkinter.messagebox.showinfo(title=None, message="The following "
-                                                        "files have been "
-                                                        "moved: {}".format(
-            moved_file_list))
+        if msg == "yes":
+            if extension in path and path not in listdir(parent):
+                os.mkdir(path)
+                print("folder created")
+                tkinter.messagebox.showinfo(title=None, message="A {} folder has "
+                                                                "been "
+                                                                "created.".format(
+                    extension))
+            moved_file_list = []
+            for file in listdir(parent):
+                if file.split(".")[-1] == extension and isdir(path):
+                    shutil.move(join(parent, file), path)
+                    moved_file_list.append(file)
+                    print("{} moved".format(file))
+            tkinter.messagebox.showinfo(title=None, message="The following "
+                                                            "files have been "
+                                                            "moved: {}".format(
+                moved_file_list))
 
 
 class Scrollbox(tkinter.Listbox):
@@ -109,14 +107,18 @@ class DataListBox(Scrollbox):
 
         if link_value and self.link_field == "files":
             self.clear()
-            for d in os.listdir(join(chosen_directory, link_value)):
+            for d in os.listdir(join(d_selector.chosen_directory,
+                                     link_value)):
                 self.insert('end', d)
 
         elif link_value and self.link_field == "extensions":
             self.clear()
             file_types = []
-            files = [f for f in listdir(join(chosen_directory, link_value)) if
-                     isfile(join("{}/{}".format(chosen_directory, link_value), f))]
+            files = [f for f in listdir(join(d_selector.chosen_directory,
+                                             link_value)) if
+                     isfile(join("{}/{}".format(d_selector.chosen_directory,
+                                                link_value),
+                                 f))]
 
             for file in files:
                 file_type = file.split(".")[-1]
@@ -126,13 +128,16 @@ class DataListBox(Scrollbox):
 
         else:
             self.clear()
-            if not chosen_directory:
-                for d in os.listdir(home):
-                    if isdir(join(home, d)) and not d.startswith('.'):
+            if not d_selector.chosen_directory:
+                for d in os.listdir(d_selector.home):
+                    if isdir(join(d_selector.home, d)) and not d.startswith(
+                            '.'):
                         self.insert('end', d)
             else:
-                for d in os.listdir(chosen_directory):
-                    if isdir(join(chosen_directory, d)) and not d.startswith('.'):
+                for d in os.listdir(d_selector.chosen_directory):
+                    if isdir(join(d_selector.chosen_directory, d)) and not \
+                            d.startswith(
+                            '.'):
                         self.insert('end', d)
 
     def on_select(self, event):
@@ -146,6 +151,7 @@ class DataListBox(Scrollbox):
 
 
 m_window = tkinter.Tk()
+d_selector = Directory_Selector()
 m_window.title('FiMo Folder Cleanup Tool')
 m_window.geometry('768x480')
 m_window.columnconfigure(0, weight=2)
@@ -163,31 +169,31 @@ tkinter.Label(m_window, text="Files and Folders").grid(row=0, column=1)
 tkinter.Label(m_window, text="Loose File Extensions").grid(row=0, column=2)
 
 # === Directories listbox ===
-directories_list = DataListBox(m_window, chosen_directory)
+directories_list = DataListBox(m_window, d_selector.chosen_directory)
 directories_list.grid(row=1, column=0, sticky='nsew', rowspan=1, padx=(30, 0))
 directories_list.config(border=2, relief='sunken')
 directories_list.requery()
 
 # === File List listbox ===
-folders_list = DataListBox(m_window, chosen_directory)
+folders_list = DataListBox(m_window, d_selector.chosen_directory)
 folders_list.grid(row=1, column=1, sticky='nsew', rowspan=1, padx=(30, 0))
 folders_list.config(border=2, relief='sunken')
 directories_list.link(folders_list, "files")
 
 # === Extensions listbox ===
-extensions_list = DataListBox(m_window, chosen_directory)
+extensions_list = DataListBox(m_window, d_selector.chosen_directory)
 extensions_list.grid(row=1, column=2, sticky='nsew', rowspan=1, padx=(30, 0))
 extensions_list.config(border=2, relief='sunken')
 directories_list.link(extensions_list, "extensions")
 
 # create extensions folder button
 new_button = tkinter.Button(m_window, text="Create Folder",
-                            command=create_folder)
+                            command=d_selector.create_folder)
 new_button.grid(row=2, column=2, sticky='new')
 
 # create file explorer button
 new_button = tkinter.Button(m_window, text="Choose Directory",
-                            command=choose_directory)
+                            command=d_selector.choose_directory)
 new_button.grid(row=2, column=0, sticky='new')
 
 
